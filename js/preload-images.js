66e541b9fabb194ebf2912fcf9f76039e813b6c2
Volosyuk Daniil <danya.volosyuk@gmail.com>
2021-09-20 01:28:53 +0300

    Added spinner

--- js/preload-images.js
+++ js/preload-images.js
@@ -1,9 +1,23 @@
 const images = [];
 
-['O', 'I', 'T', 'S', 'Z', 'J', 'L'].forEach(letter => {
-    for (let i = 0; i < 4; i++) {
+(() => {
+    const imgUrls = [];
+    let totalImagesLoaded = 0;
+
+    ['O', 'I', 'T', 'S', 'Z', 'J', 'L'].forEach(letter => {
+        for (let i = 0; i < 4; i++)
+            imgUrls.push(`${FIGURES_PATH}${letter}${i}.png`);
+    });
+
+    imgUrls.forEach(url => {
         const img = new Image();
-        img.src = `${FIGURES_PATH}${letter}${i}.png`;
+        img.onload = () => {
+            if (++totalImagesLoaded >= imgUrls.length) 
+                document.getElementById('loader').style.opacity = '0';
+        };
+        img.src = url;
         images.push(img);
-    } 
-});
+    })
+})()
+
+
